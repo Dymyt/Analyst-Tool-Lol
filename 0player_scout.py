e36c70451a08154ead023ca799e180b16c85fea9
WildeHack <34816097+Dymyt@users.noreply.github.com>
2020-08-25 10:41:49 +0200

    Update 0player_scout.py

--- 0player_scout.py
+++ 0player_scout.py
@@ -2,6 +2,8 @@ import aux_functions
 import functions
 import excel_functions
 
+##Example for getting all the info from a player within 1 month
+
 period1, period2, period3, period4 = functions.generatePlayerListMatchClass("Devyluke", "01-07-2020")
 
 print(period1, period2, period3, period4)
